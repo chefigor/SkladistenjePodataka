d288efc25cf639a16ff4928298137bf602cae483
Igor Jovanovic <igorjovanovic2898@gmail.com>
2021-05-05 15:04:57 +0200

    first commit

---
+++ SQL/Car_Manufacturers.sql
@@ -0,0 +1,6 @@
+create table Car_Manufacturers (
+	manufacturer_Shortname VARCHAR(10),
+	manufacturer_Fullname VARCHAR(25),
+	manufacturer_Otherdetails VARCHAR(50),
+	PRIMARY KEY(manufacturer_Shortname)
+);
