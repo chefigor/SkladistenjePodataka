7e901dfa1b3238f1e1a5d889106a62a5a2355496
Igor Jovanovic <igorjovanovic2898@gmail.com>
2021-05-13 18:12:43 +0200

    KTR transformations

--- SQL/Car_Manufacturers.sql
+++ SQL/Car_Manufacturers.sql
@@ -4,3 +4,13 @@ create table Car_Manufacturers (
 	manufacturer_Otherdetails VARCHAR(50),
 	PRIMARY KEY(manufacturer_Shortname)
 );
+
+manufacturer_Shortname	manufacturer_Fullname	manufacturer_Otherdetails
+BMW	Devbug	Digitized optimal workforce
+Chevrolet	Twitterlist	Future-proofed 5th generation instruction set
+Dodge	Linktype	Reactive web-enabled benchmark
+Ford	Podcat	Right-sized needs-based protocol
+Honda	Eayo	Optimized intangible task-force
+Lexus	Skiba	Expanded interactive orchestration
+Mazda	Twimbo	Centralized 24/7 knowledge user
+Toyota	Jabbertype	Networked client-server implementation
